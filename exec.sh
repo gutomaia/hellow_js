f157cc0e8e064d38bf4f03bf424444aaedb0711a
Guto Maia <guto@guto.net>
2010-05-27 20:22:57 -0300

    deleting old execute methods

--- exec.sh
+++
@@ -1,8 +0,0 @@
-rm msn.js
-cat	lib/md5_crypt.js \
-	src/hellow/protocol/msnp.js \
-	src/hellow/protocol/notification.js \
-	src/hellow/protocol/msnp8.js \
-	test/protocolTest.js >> msn.js
-rhino msn.js
-	
--- exec2.sh
+++
@@ -1,7 +0,0 @@
-rm msn.js
-cat	lib/md5_crypt.js \
-	src/hellow/protocol/msnp.js \
-	src/hellow/protocol/notification.js \
-	src/hellow/protocol/msnp8.js >> msn.js
-rhino msn.js
-	
