f6f62771203561ee318b74a42bf3a486ddf32419
Guto Maia <guto@guto.net>
2011-07-17 13:42:35 -0300

    adding the message_test

--- src/test/javascript/message_test.js
+++ src/test/javascript/message_test.js
@@ -7,59 +7,54 @@
  *  Human Knowledge belongs to the World!
  *--------------------------------------------------------------------------*/
 
-function MessageTest(name){
-	TestCase.call(this, name);
-}
+MessageTest = TestCase("MessageTest");
 
-MessageTest.prototype = new TestCase();
-MessageTest.glue();
-
-function MessageTest_testMsgReveiceTypingUser() {
+MessageTest.prototype.testMsgReveiceTypingUser = function () {
 	var command = "MSG dvader@empire.com Vader 90\r\n"
 			+ "MIME-Version: 1.0\r\n"
 			+ "Content-Type: text/x-msmsgscontrol\r\n"
 			+ "TypingUser: dvader@empire.com\r\n" + "\r\n" + "\r\n";
 	var message = new Message(command);
-	this.assertNotNull(message.getHeader());
-	this.assertEquals("1.0", message.getHeaderParameter("MIME-Version"));
-	this.assertEquals("text/x-msmsgscontrol", message.getHeaderParameter("Content-Type"));
-	this.assertEquals("dvader@empire.com", message.getHeaderParameter("TypingUser"));
-	this.assertEquals("\r\n", message.getBody());
+	assertNotNull(message.getHeader());
+	assertEquals("1.0", message.getHeaderParameter("MIME-Version"));
+	assertEquals("text/x-msmsgscontrol", message.getHeaderParameter("Content-Type"));
+	assertEquals("dvader@empire.com", message.getHeaderParameter("TypingUser"));
+	assertEquals("\r\n", message.getBody());
 }
 
-function MessageTest_testMsgReceiveHello() {
+MessageTest.prototype.testMsgReceiveHello = function () {
 	var command = "MSG dvader@empire.com Vader 143\r\n"
 			+ "MIME-Version: 1.0\r\n"
 			+ "Content-Type: text/plain; charset=UTF-8\r\n"
 			+ "X-MMS-IM-Format: FN=Lucida%20Sans%20Unicode; EF=B; CO=ff0000; CS=0; PF=22\r\n"
 			+ "\r\n" + "Hello.";
 	var message = new Message(command);
-	this.assertNotNull(message.getHeader());
-	this.assertEquals("1.0", message.getHeaderParameter("MIME-Version"));
-	this.assertEquals("text/plain; charset=UTF-8",message.getHeaderParameter("Content-Type"));
-	this.assertEquals("FN=Lucida%20Sans%20Unicode; EF=B; CO=ff0000; CS=0; PF=22",message.getHeaderParameter("X-MMS-IM-Format"));
-	this.assertEquals("Hello.", message.getBody());
+	assertNotNull(message.getHeader());
+	assertEquals("1.0", message.getHeaderParameter("MIME-Version"));
+	assertEquals("text/plain; charset=UTF-8",message.getHeaderParameter("Content-Type"));
+	assertEquals("FN=Lucida%20Sans%20Unicode; EF=B; CO=ff0000; CS=0; PF=22",message.getHeaderParameter("X-MMS-IM-Format"));
+	assertEquals("Hello.", message.getBody());
 }
 
-function MessageTest_testMsgSendTypingUser() {
+MessageTest.prototype.testMsgSendTypingUser = function () {
 	var command = "MSG 2 U 90\r\n" + "MIME-Version: 1.0\r\n"
 			+ "Content-Type: text/x-msmsgscontrol\r\n"
 			+ "TypingUser: dvader@empire.com\r\n" + "\r\n" + "\r\n";
 	var message = new Message(null, 2, "dvader@empire.com");
-	this.assertEquals(command, message.send());
+	assertEquals(command, message.send());
 }
 
-function MessageTest_testMsgSendAreYouThere() {
+MessageTest.prototype.testMsgSendAreYouThere = function () {
 	var command = "MSG 5 N 138\r\n"
 			+ "MIME-Version: 1.0\r\n"
 			+ "Content-Type: text/plain; charset=UTF-8\r\n"
 			+ "X-MMS-IM-Format: FN=MS%20Sans%20Serif; EF=; CO=0; CS=0; PF=0\r\n"
 			+ "\r\n" + "Are you there?";
 	var message = new Message("Are you there?", 5);
-	this.assertEquals(command, message.send());
+	assertEquals(command, message.send());
 }
 
-function MessageTest_testMsnSendILikeTurtles(){
+MessageTest.prototype.testMsnSendILikeTurtles = function (){
 	var command = "MSG 8 A 139\r\n"+
     "MIME-Version: 1.0\r\n"+
     "Content-Type: text/plain; charset=UTF-8\r\n"+
