4bc9a9fb4fc77dcd73865c476ac006d7ec65df3f
Guto Maia <guto@guto.net>
2011-07-17 13:13:28 -0300

    first test running with js-test-driver

--- src/test/javascript/protocol_test.js
+++ src/test/javascript/protocol_test.js
@@ -7,23 +7,17 @@
  *  Human Knowledge belongs to the World!
  *--------------------------------------------------------------------------*/
 
-function ProtocolTest(name){
-	TestCase.call(this, name);
-	this._received = null;
-}
-
-ProtocolTest.prototype = new TestCase();
-ProtocolTest.glue();
+ProtocolTest = TestCase("ProtocolTest");
 
 ProtocolTest.prototype.send = function (msg) {
-	this.assertEquals("Send command is invalid", msg, this._mockConnection.sended);
+	assertEquals("Send command is invalid", msg, this._mockConnection.sended);
 }
 
 ProtocolTest.prototype.receive = function (msg) {
 	this._msn.execute(msg);	
 }
 
-function ProtocolTest_setUp(){
+ProtocolTest.prototype.setUp = function (){
 	this._mockClient = new MockClient();
 	this._mockAuth = new MockAuthentication();
 	this._mockConnection = new MockConnection();
@@ -35,13 +29,13 @@ function ProtocolTest_setUp(){
 	this._msn.login("dvader@empire.com", "ih8jedis");
 }
 
-function ProtocolTest_testChallenger(){
+ProtocolTest.prototype.testChallenger = function (){
 	var chl = "29409134351025259292";
 	var digest = this._msn.challenger(chl);
-	this.assertEquals("d0c1178c689350104350d99f8c36ed9c", digest);
+	assertEquals("d0c1178c689350104350d99f8c36ed9c", digest);
 }
 
-function ProtocolTest_testSession(){
+ProtocolTest.prototype.testSession = function () {
 	//Sends the MSN Client version
 	//this.send("VER 1 MSNP8 CVR0\r\n");
 
@@ -56,19 +50,19 @@ function ProtocolTest_testSession(){
 	//Redirect
 	this.receive("XFR 3 NS 207.46.106.118:1863 0 207.46.104.20:1863\r\n");
 	this.send("VER 4 MSNP8 CVR0\r\n");
-	this.assertEquals("Invalid host redefinition", "207.46.106.118",this._mockConnection.host);
-	this.assertEquals("Invalid port redefinition", "1863",this._mockConnection.port);
+	assertEquals("Invalid host redefinition", "207.46.106.118",this._mockConnection.host);
+	assertEquals("Invalid port redefinition", "1863",this._mockConnection.port);
 	this.receive("VER 4 MSNP8 CVR0\r\n");
 	this.send("CVR 5 0x0409 win 4.10 i386 MSNMSGR 6.0.0602 MSMSGS dvader@empire.com\r\n");
 	this.receive("CVR 5 6.0.0602 6.0.0602 1.0.0000 http://download.microsoft.com/download/8/a/4/8a42bcae-f533-4468-b871-d2bc8dd32e9e/SETUP9x.EXE http://messenger.msn.com\r\n");
 	this.send("USR 6 TWN I dvader@empire.com\r\n");
 	this.receive("USR 6 TWN S lc=1033,id=507,tw=40,fs=1,ru=http%3A%2F%2Fmessenger%2Emsn%2Ecom,ct=1062764229,kpp=1,kv=5,ver=2.1.0173.1,tpf=43f8a4c8ed940c04e3740be46c4d1619\r\n");
 	this.send("USR 7 TWN S t=53*1hAu8ADuD3TEwdXoOMi08sD*2!cMrntTwVMTjoB3p6stWTqzbkKZPVQzA5NOt19SLI60PY!b8K4YhC!Ooo5ug$$&p=5eKBBC!yBH6ex5mftp!a9DrSb0B3hU8aqAWpaPn07iCGBw5akemiWSd7t2ot!okPvIR!Wqk!MKvi1IMpxfhkao9wpxlMWYAZ!DqRfACmyQGG112Bp9xrk04!BVBUa9*H9mJLoWw39m63YQRE1yHnYNv08nyz43D3OnMcaCoeSaEHVM7LpR*LWDme29qq2X3j8N\r\n");
-	this.assertFalse("User not logged, ConnectionListener::onLogged shoudn't be called", this._mockClient.logged);
+	assertFalse("User not logged, ConnectionListener::onLogged shoudn't be called", this._mockClient.logged);
 	
 	//Logged
 	this.receive("USR 7 OK dvader@empire.com Dart%20Vader 1 0\r\n");
-	this.assertTrue("User logged, ConnectionListener::onLogged should be called",this._mockClient.logged);
+	assertTrue("User logged, ConnectionListener::onLogged should be called",this._mockClient.logged);
 	this.send("SYN 1 0\r\n");
 	this.receive("SYN 8 27 5 4\r\n");
 	this.receive("GTC A\r\n");
@@ -76,34 +70,34 @@ function ProtocolTest_testSession(){
 	this.receive("PRP PHH O1%20234\r\n");
 	this.receive("PRP PHM 56%20789\r\n");
 	
-	this.assertEquals(this._mockClient.group, null);
+	assertEquals(this._mockClient.group, null);
 	this.receive("LSG 0 Sifth\r\n");
-	this.assertNotNull('Group is null and should be setted as Sifth', this._mockClient.group);
-	this.assertEquals('Invalid group id', '0', this._mockClient.group.group_id);
-	this.assertEquals('Invalid group name', 'Sifth', this._mockClient.group.name);
+	assertNotNull('Group is null and should be setted as Sifth', this._mockClient.group);
+	assertEquals('Invalid group id', '0', this._mockClient.group.group_id);
+	assertEquals('Invalid group name', 'Sifth', this._mockClient.group.name);
 	this.receive("LSG 1 Jedis\r\n");
-	this.assertNotNull('Group is null and should be setted as Jedis', this._mockClient.group);
-	this.assertEquals('Invalid group id', '1', this._mockClient.group.group_id);
-	this.assertEquals('Invalid group name', 'Jedis', this._mockClient.group.name);
+	assertNotNull('Group is null and should be setted as Jedis', this._mockClient.group);
+	assertEquals('Invalid group id', '1', this._mockClient.group.group_id);
+	assertEquals('Invalid group name', 'Jedis', this._mockClient.group.name);
 	
 	//Add Emperor as a contact
-	this.assertEquals(this._mockClient.contact, null);
+	assertEquals(this._mockClient.contact, null);
 	this.receive("LST emperor@empire.com Emperor 13 0\r\n");
-	this.assertNotNull('Contact is null and should be setted as emperor@empire.com', this._mockClient.contact);
-	this.assertEquals('emperor@empire.com', this._mockClient.contact.user);
-	this.assertEquals('Emperor', this._mockClient.contact.nick);
-	this.assertEquals('13', this._mockClient.contact.lists);
-	this.assertEquals('0', this._mockClient.contact.groups);
+	assertNotNull('Contact is null and should be setted as emperor@empire.com', this._mockClient.contact);
+	assertEquals('emperor@empire.com', this._mockClient.contact.user);
+	assertEquals('Emperor', this._mockClient.contact.nick);
+	assertEquals('13', this._mockClient.contact.lists);
+	assertEquals('0', this._mockClient.contact.groups);
 	this.receive("BPR MOB Y\r\n");
 	
 	//Add Luke as a contact
 	this._mockClient.contact = null;
 	this.receive("LST luke@rebels.org Luke 3 1\r\n");
-	this.assertNotNull('Contact is null and should be setted as luke@rebels.org', this._mockClient.contact);
-	this.assertEquals(this._mockClient.contact.user, 'luke@rebels.org');
-	this.assertEquals(this._mockClient.contact.nick, 'Luke');
-	this.assertEquals(this._mockClient.contact.lists, '3'); //Luke dosen't have Vader in their list! Bastard!!
-	this.assertEquals(this._mockClient.contact.groups, '1');
+	assertNotNull('Contact is null and should be setted as luke@rebels.org', this._mockClient.contact);
+	assertEquals(this._mockClient.contact.user, 'luke@rebels.org');
+	assertEquals(this._mockClient.contact.nick, 'Luke');
+	assertEquals(this._mockClient.contact.lists, '3'); //Luke dosen't have Vader in their list! Bastard!!
+	assertEquals(this._mockClient.contact.groups, '1');
 
 	this.send("CHG 9 NLN 0\r\n");
 	this.receive("CHG 9 NLN 0\r\n");
